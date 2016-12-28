85fa7b1aa2040b53543b1263ac7df87c72dc8ef7
Ruslan Variushkin <ruslan@host4.biz>
2016-12-28 16:40:33 +0100

    initial commit

--- lib/SB.py
+++ lib/SB.py
@@ -10,12 +10,10 @@ global MongoConnect
 global DBs
 global Collection
 global Num_thread
-global DIR
 global DirBackup
 MongoConnect = config.get('Main', 'MongoConnect')
 DBs = config.get('Main',  'DBs')
 Collection = config.get('Main', 'Collection')
 Num_thread = int(config.get('Main',  'Num_thread'))
-DIR = config.get('Main',  'DIR')
 DirBackup = config.get('Main', 'DirBackup')
 
