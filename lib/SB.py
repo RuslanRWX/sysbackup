42ae6dd926cbf27af0c06b410e8bc39b792c9b5e
Ruslan Variushkin <ruslan@host4.biz>
2017-05-10 15:02:00 +0200

    initial commit

--- lib/SB.py
+++ lib/SB.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 import ConfigParser,  os
 
 Pathini=os.path.dirname(__file__), 'sbd.ini'
@@ -10,9 +10,10 @@ global DBs
 global Collection
 global Num_thread
 global DirBackup
+global Pidfile
 MongoConnect = config.get('Main', 'MongoConnect')
 DBs = config.get('Main',  'DBs')
 Collection = config.get('Main', 'Collection')
 Num_thread = int(config.get('Main',  'Num_thread'))
 DirBackup = config.get('Main', 'DirBackup')
-
+Pidfile = config.get('Main', 'Pidfile')
