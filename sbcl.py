42d9c1ad36692a2d1d830bddd01911f91debb147
Ruslan Variushkin <ruslan@host4.biz>
2017-06-16 23:10:17 +0200

    initiall commit

--- sbcl.py
+++ sbcl.py
@@ -324,7 +324,8 @@ def help():
     return """Help function: Basic Usage:
     \tmysqldump     - Start mysqldump
     \tlist          - list,show your configurations 
-    \tbackup         - Add status needbackup, it mean the backup server will start a backup script as fast as it can
+    \tbackup        - Add status needbackup, it mean the backup server will start a backup script as fast as it can
+    \treconf        - Reconfiguration of backup settings 
     """
 
 
@@ -338,7 +339,7 @@ if __name__ == '__main__':
             
             print "Send command to backup"
             GetData("Add|Status|needbackup")
-        elif sys.argv[1] == 'update':
+        elif sys.argv[1] == 'reconf':
             update()
         else:
             print help()
